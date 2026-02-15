7d6ca3948e788f3813b0c9cda0c080d3ef43c736
ironiccapitals <ironiccapitals@gmail.com>
2026-02-15 10:52:42 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,80 +15,51 @@
     // Initiate the wowjs
     new WOW().init();
 
+
     // Sticky Navbar
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
+        if ($(window).width() > 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.sticky-top .container').addClass('shadow-sm').css('max-width', '100%');
+            } else {
+                $('.sticky-top .container').removeClass('shadow-sm').css('max-width', $('.topbar .container').width());
+            }
         } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
+            $('.sticky-top .container').addClass('shadow-sm').css('max-width', '100%');
         }
     });
 
 
     // Hero Header carousel
     $(".header-carousel").owlCarousel({
-        animateOut: 'fadeOut',
         items: 1,
-        margin: 0,
-        stagePadding: 0,
         autoplay: true,
-        smartSpeed: 500,
-        dots: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: false,
         loop: true,
+        margin: 0,
         nav : true,
         navText : [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
-        ],
+        ]
     });
 
 
-    // attractions carousel
-    $(".blog-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: false,
-        dots: false,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-right"></i>',
-            '<i class="fa fa-angle-left"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:3
-            }
-        }
-    });
 
-
-    // testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
+    // Project carousel
+    $(".project-carousel").owlCarousel({
         autoplay: true,
-        smartSpeed: 1500,
+        smartSpeed: 1000,
         center: false,
         dots: true,
         loop: true,
         margin: 25,
-        nav : true,
+        nav : false,
         navText : [
-            '<i class="fa fa-angle-right"></i>',
-            '<i class="fa fa-angle-left"></i>'
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
         ],
         responsiveClass: true,
         responsive: {
@@ -105,7 +76,7 @@
                 items:2
             },
             1200:{
-                items:3
+                items:2
             }
         }
     });
@@ -118,6 +89,7 @@
     });
 
 
+    
    // Back to top button
    $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
